ade004816fc5678b93b85bd10cb2a0a6154b96fe
root <n.ntare@alustudent.com>
2024-10-05 02:32:20 -0700

    add files

--- create_environment.sh
+++ create_environment.sh
@@ -3,10 +3,12 @@ mkdir -p submission_reminder_app/{app,modules,assets,config}
 
 cd submission_reminder_app
 
-cat /shell_scripting_lab/downloaded-files/reminder.sh > app/reminder.sh && chmod +x reminder.sh
-cat /shell_scripting_lab/downloaded-files/functions.sh > modules/functions.sh && chmod +x functions.sh
+
+
+cat /shell_scripting_lab/downloaded-files/reminder.sh > app/reminder.sh && chmod +x app/reminder.sh
+cat /shell_scripting_lab/downloaded-files/functions.sh > modules/functions.sh && chmod +x modules/functions.sh
 cat /shell_scripting_lab/downloaded-files/config.env > config/config.env
-cat /shell_scripting_lab/submisions.txt > assets/submissions.txt
+cat /shell_scripting_lab/submissions.txt > assets/submissions.txt
 
    echo -e "Ntare, Shell Navigation, submitted\nNoella, Shell Navigation, Not submitted\nherve, Shell Navigation, submitted\nTeta, Shell Navigation, Not submitted\nDelucie, Shell Navigation, submitted" >> assets/submitted.txt
 
